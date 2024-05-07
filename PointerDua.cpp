3f59cb709c3b2712a0ddc2e8e2e5217d5d67bc9b
dzakyputra45 <dzakypratama45@gmail.com>
2024-05-07 15:20:29 +0700

    Membuat flow menampilkan mahasiswa

--- PointerDua.cpp
+++ PointerDua.cpp
@@ -10,3 +10,17 @@ class mahasiswa {
     }
 };
 
+int main (){
+
+    mahasiswa mhs {1};      //object mhs
+    mhs.showNim();          //Member access operator
+
+    mahasiswa &refMhs = mhs;    //pointer reference refMhs
+    refMhs.nim = 2;             //Member access operator
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs;     //pointer deference pMhs
+    pMhs->nim = 3;              //Arrow Operator
+    pMhs->showNim();
+    return 0;
+}
